f779e70682c119cc6911a19781c0cfe0073613b0
tjellis19 <54557118+tjellis19@users.noreply.github.com>
2019-09-30 23:19:08 -0400

    Add files via upload

---
+++ Staff.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_review2;
+
+/**
+ *
+ * @author tjell
+ */
+public class Staff {
+    //--------- Variables ---------\\
+    
+    
+    //--------- Constructor(s) ---------\\
+    public Staff()
+    {
+        
+    }
+    //--------- Setters and Gettersv---------\\
+    
+    
+    
+    //--------- Additional Methods ---------\\
+    public String toString()
+    {
+       return " ";
+    }
+}// end of class
